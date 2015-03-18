6436b9192ab027fcdd608e94cd5ecd009f567764
Selim Ober <selim.ober@gmail.com>
2015-03-18 18:43:58 +0200

    lecture 1

---
+++ lectures/1/test.hs
@@ -0,0 +1,14 @@
+double x = x + x
+
+quad x = double (double x)
+
+factorial n = product [1..n]
+
+average ns = sum ns `div` length ns
+
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
